3f26fd9dcae28c83ca6db1ee29ba590463b07879
ali-wetrill <ali.ahmed@berkeley.edu>
2022-05-16 14:19:31 +0000

    added some extra goodies in the ports.yaml

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -19,12 +19,16 @@ sudo apt-get install -y nodejs
 ##### MongoDB #####
 (echo ""; echo "##### MongoDB v4.4 #####"; echo "";)
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
 ## operation should respond with OK 
 ## if you receive an error indicating that gnupg is not installed, you can - then run wget commd again
 ## sudo apt-get install gnupg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo systemctl start mongod # starting mongo locally
+sudo systemctl status mongod # checking status
+# to run the mongo shell, just type in "mongo" in your terminal
 
 ##### Custom commands #####
 (echo ""; echo "##### Custom commands #####"; echo "";)
